d64c392192e1af1f30c73aab5398e69585d1a84f
samarth jagakar <samarth.skillsconnect@gmail.com>
2025-07-14 14:54:50 +0530

    screen-issue-fixed

--- book-demo-app/backend/src/middleware/adminAuth.js
+++ book-demo-app/backend/src/middleware/adminAuth.js
@@ -1,34 +1,4 @@
 
-// module.exports = function adminAuth(req, res, next) {
-//   const adminToken = req.headers['x-admin-token'];
-//   if (adminToken && adminToken === process.env.ADMIN_TOKEN) {
-//     return next();
-//   }
-//   res.status(401).json({ message: 'Unauthorized' });
-// };
-
-
-
-
-
-// const jwt = require('jsonwebtoken');
-
-// module.exports = function (req, res, next) {
-//   const token = req.header('Authorization')?.split(' ')[1];
-//   if (!token) return res.status(401).json({ message: "Access denied. No token provided." });
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded;
-//     next();
-//   } catch {
-//     res.status(400).json({ message: "Invalid token." });
-//   }
-// };
-
-
-
-
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
